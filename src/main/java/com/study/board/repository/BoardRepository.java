26fc951c5a89fa63f2fbf0f9a57faac6b422fc08
choyeseol <choyeseol07@gmail.com>
2024-02-02 16:40:17 +0900

    feat: 검색 기능 추가

--- src/main/java/com/study/board/repository/BoardRepository.java
+++ src/main/java/com/study/board/repository/BoardRepository.java
@@ -7,4 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BoardRepository extends JpaRepository<Board, Integer> {
     Page<Board> findAll(Pageable pageable);
+    Page<Board> findByTitleContaining(String searchKeyword, Pageable pageable);
 }
