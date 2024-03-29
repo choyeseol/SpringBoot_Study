a3e9501018b194caff7281180ed6baa866dfd770
choyeseol <choyeseol07@gmail.com>
2024-03-29 13:51:11 +0900

    .

--- src/main/java/com/study/board/BoardApplication.java
+++ src/main/java/com/study/board/BoardApplication.java
@@ -8,9 +8,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @SpringBootApplication
 @EnableJpaRepositories
 public class BoardApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(BoardApplication.class, args);
 	}
-
 }
