cbb688325cfc5e686dce832a79eb2137be988e91
choyeseol <choyeseol07@gmail.com>
2024-03-29 14:06:48 +0900

    Initial commit

--- src/main/java/com/study/board/domain/entity/Board.java
+++ src/main/java/com/study/board/domain/entity/Board.java
@@ -6,6 +6,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Data
@@ -21,4 +22,5 @@ public class Board {
     private String filename;
 
     private String filepath;
+
 }
